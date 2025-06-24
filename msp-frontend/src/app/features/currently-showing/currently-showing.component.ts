5c9c2616ecbe27721832bc0f78efa7cc365eb8a9
Enoch Whitaker <ewhitaker@DHICorp.com>
2025-06-24 10:36:51 -0600

    Can get show by Id

--- msp-frontend/src/app/features/currently-showing/currently-showing.component.ts
+++ msp-frontend/src/app/features/currently-showing/currently-showing.component.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { environment } from '../../../environments/environment';
 import { Play } from '../../interfaces/play';
 import { LoaderDarkComponent } from '../../shared/loader-dark/loader-dark.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-currently-showing',
@@ -16,6 +17,9 @@ export class CurrentlyShowingComponent {
   currentPlay!: Play;
   loading = true;
   baseUrl = environment.apiUrl;
+  playId: string | null = null;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     fetch(`${this.baseUrl}/GetPlays`)
@@ -26,6 +30,10 @@ export class CurrentlyShowingComponent {
       return response.json();
     })
     .then(data => {
+      this.playId = this.route.snapshot.paramMap.get('id');
+      if (this.playId) {
+        data.value = data.value.filter((play: Play) => play.id === Number(this.playId));
+      }
       this.currentPlay = data.value[0];
       this.loading = false;
     })
